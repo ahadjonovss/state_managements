1948394c592a6611eac628a5e9727462970263c5
Asus <samandarahadjonovss@gmail.com>
2023-02-21 15:10:04 +0500

    provider 1st lesson

---
+++ lib/2-lesson/ui/description/description_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class DescriptionPage extends StatelessWidget {
+  const DescriptionPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Description Page"),
+      ),
+    );
+  }
+}
